ca6f03fc51e41b0e6ac3febe63e77098683f36ad
lzddarko <lzddarko@gmail.com>
2025-06-18 14:44:16 +0200

    Added GitHub page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+
+const inProduction = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: inProduction ? "/unlimited-wordle" : "",
+  assetPrefix: inProduction ? "/unlimited-wordle/" : ""
 };
 
 export default nextConfig;
